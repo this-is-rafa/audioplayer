99d898b2fa9c4d8c7fb93e35ad676aec741af57d
this-is-rafa <rafa@ratradio.net>
2018-05-21 12:00:40 -0400

    Config ready

--- gulpfile.js
+++ gulpfile.js
@@ -19,59 +19,31 @@ var cleanCssOptions = {
   level: 2
 };
 
-
 gulp.task('sassy', function() {
   return gulp
-    .src('./scss/style.scss')
+    .src('./src/scss/style.scss')
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(cleanCss(cleanCssOptions))
-    .pipe(gulp.dest('./app/'));
+    .pipe(gulp.dest('./dist/css/'));
 })
 
 gulp.task('html', function () {
-  return gulp.src([
-    './app/header.html', 
-    './svg/icons.html', 
-    './app/client.html',
-    './app/toolbar.html',
-    './app/alert_bar.html',
-    './app/content-header.html',
-    './app/profilebar.html',
-    './app/profile.html',
-    './app/status.html',
-    './app/patient_status.html',
-    './app/room_history.html',
-    './app/operating_room.html',
-    './app/or_performance.html',
-    './app/or_schedule.html',
-    './app/hawthorne.html',
-    './app/efficiency.html',
-    './app/case_menu.html',
-    './app/case_room_first.html',
-    './app/case_room_last.html ',
-    './app/case_per_day.html',
-    './app/patient_flow.html',
-    './app/patient_location.html',
-    './app/room_occupied.html',
-    './app/room_utilization.html',
-    './app/room_turnaround.html',
-    './app/tag_registered.html',
-    './app/wristband.html',
-    './app/notification.html',
-    './app/notification_history.html',
-    './app/patient.html',
-    './app/settings.html',
-    './app/content-footer.html',
-    './app/footer.html',
-  ])
+  return gulp.src('./src/index.html')
     .pipe(concat('index.html'))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['html', 'sassy']);
+gulp.task('js', function () {
+  return gulp.src("./src/audioplayer.js")
+    .pipe(babel())
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('default', ['html', 'sassy', 'js']);
 
 gulp.task('watch', function () {
-  gulp.watch('app/*.html', ['html']);
-  gulp.watch('scss/*.scss', ['sassy']);
+  gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/scss/*.scss', ['sassy']);
+  gulp.watch('src/js/*.js', ['js']);
 });
